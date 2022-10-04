a1443ed4aa3b22d9a84af4e88aa4b2941ceb1323
Christopher Negron <sonycar2000@outlook.com>
2022-10-04 15:15:15 -0400

    Data loaded into memory

---
+++ src/main/java/com/soit/soitfaculty/FacultyController.java
@@ -0,0 +1,42 @@
+package com.soit.soitfaculty;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import com.soit.soitfaculty.model.Faculty;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/Faculties")
+public class FacultyController {
+	/**
+	 * 
+	 */
+	//upload Faculty info
+	private List<Faculty> theFaculties;
+	
+	@PostConstruct
+	private void loadData() {
+		
+		//Create falculties
+		Faculty fac1 = new Faculty(1, "Kelly", "Miller", "kelly@uc.edu");
+		Faculty fac2 = new Faculty(2, "Robbert", "Lee", "Robbert@uc.edu");
+		Faculty fac3 = new Faculty(3, "laura", "West", "laura@uc.edu");
+		
+		//create our list
+		theFaculties = new ArrayList();
+		
+		//add to our list
+		theFaculties.add(fac1);
+		theFaculties.add(fac2);
+		theFaculties.add(fac3);
+		
+		
+	}
+	
+	
+	//Mapping for '/list'
+	
+}
