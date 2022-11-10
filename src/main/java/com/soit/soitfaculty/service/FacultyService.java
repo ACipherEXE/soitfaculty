a1ca093bf4ec1124773bb88361d5d622ac895b5a
Christopher Negron <sonycar2000@outlook.com>
2022-11-10 14:51:30 -0500

    Database done

--- src/main/java/com/soit/soitfaculty/service/FacultyService.java
+++ src/main/java/com/soit/soitfaculty/service/FacultyService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import com.soit.soitfaculty.entity.Faculty;
 
 public interface FacultyService {
+	
 	public List<Faculty> findAll();
 	public Faculty findById(int theId);
 	public void save(Faculty theFaculty);
