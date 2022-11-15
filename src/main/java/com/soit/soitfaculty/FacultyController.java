ecef2f9c2ca618dd39ec1a9d9a57ac1b9a97ba3b
Christopher Negron <sonycar2000@outlook.com>
2022-11-15 15:25:00 -0500

    Added a way to add more faculties

--- src/main/java/com/soit/soitfaculty/FacultyController.java
+++ src/main/java/com/soit/soitfaculty/FacultyController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.soit.soitfaculty.entity.Faculty;
@@ -51,7 +53,21 @@ public class FacultyController {
 		List<Faculty> theFaculties = facultyService.findAll();
 		//Add Faculties to the spring model 
 		theModel.addAttribute("faculties", theFaculties);
-		return "list-faculties";
+		return "faculties/list-faculties";
 	}
-	
+	@GetMapping("/viewAddForm")
+	public String viewAddForm(Model theModel) {
+		//Model attribute for data binding
+		Faculty theFaculty = new Faculty();
+		theModel.addAttribute("faculty", theFaculty);
+		return "faculties/faculty-form";
+	}
+	@PostMapping("/save")
+	public String saveFaculty(@ModelAttribute("faculty") Faculty theFaculty) {
+		//Register the faculty 
+		facultyService.save(theFaculty);
+		//block the duplicates submission for accidental refresh
+		
+		return "redirect:/Faculties/list";
+	}
 }
